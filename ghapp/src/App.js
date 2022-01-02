4876a0182421b8d9a50fdf93e7563492f729326e
ankushjhakda <ankush1303.air@gmail.com>
2022-01-02 21:39:26 +0530

    added base original files

---
+++ ghapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Title Links will be here
+      </header>
+      <body>
+        Main document will be here soon !
+      </body>
+      <footer>
+        2022 &copy; ankush jhakda
+        </footer>  
+    </div>
+  );
+}
+
+export default App;
